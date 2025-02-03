999b2d3b77f7680c6b0a03331f3cead7ccbd5897
Laurent Ailleres <laurent.ailleres@pgn.com.au>
2025-02-03 13:37:50 +1100

    ebird

--- 3Dconference2025-ebird.js
+++ 3Dconference2025-ebird.js
@@ -34,7 +34,7 @@ let paragraph = (function () {
 	pub.setup = () => {
 		$.ajax({
 			type: "GET",
-			url: "3Dconference2025.json",
+			url: "3Dconference2025-ebird.json",
 			data: {
 				get_param: "value",
 			},
